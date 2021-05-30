b82f69300a4f961333c0427ef99430a70dc2b1f2
alexuglav <57800797+alexuglav@users.noreply.github.com>
2021-05-30 16:26:00 +0500

    Create Error.h

---
+++ Lab18/Lab18_9/Error.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class error
+{
+	string str;
+public:
+	error(string s) { str = s; }
+	void what() { cout << str << endl; }
+};
